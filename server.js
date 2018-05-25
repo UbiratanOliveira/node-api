f23da96ce197e375f2214a5ff0332fbd4b3cf3b3
Ubiratan <ubiratan.comp@outlook.com>
2018-05-25 19:22:05 -0300

    Starting debug

--- server.js
+++ server.js
@@ -21,6 +21,7 @@ app.use('/', route);
 
 server.listen(port);
 server.on('error', onError);
+server.on('listening', onListening);
 
 console.log('API is running on port ' + port);
 
@@ -62,4 +63,12 @@ function onError(error) {
             throw error;
     }
 
+}
+
+function onListening() {
+    const addr = server.address();
+    const bind = typeof addr === 'string'
+        ? 'Pipe ' + addr
+        : 'Port ' + addr.port;
+    debug('Listening on ' + bind);
 }
